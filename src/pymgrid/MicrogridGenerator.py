ec107f001264ac8dbe364c179ceced36c8d43b63
lbaitemple <lbai@temple.edu>
2023-02-05 12:08:12 -0500

    Update MicrogridGenerator.py

--- src/pymgrid/MicrogridGenerator.py
+++ src/pymgrid/MicrogridGenerator.py
@@ -22,7 +22,7 @@ You should have received a copy of the GNU Lesser General Public License along w
 import numpy as np
 import pandas as pd
 from pymgrid import NonModularMicrogrid, PROJECT_PATH
-import pymgrid.Microgrid
+#import pymgrid.Microgrid
 from os import listdir
 from os.path import isfile, join
 import os
